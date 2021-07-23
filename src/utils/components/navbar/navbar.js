046477106ef5b749c2958d9f6f043f67cf3f1e89
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-23 15:35:04 -0500

    Feat: Agrega lectura de estado de contexto en navbar para probar el acceso a los mismos

--- src/utils/components/navbar/navbar.js
+++ src/utils/components/navbar/navbar.js
@@ -1,8 +1,12 @@
-import { React } from 'react';
+import { React,useContext } from 'react';
 import { NavLink } from "react-router-dom";
+
+import { UserContext } from '@contexts/UserContext/UserContext';
 
 
 export default function Navbar(){
+    const { Name } = useContext(UserContext);
+    console.log(Name)
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
