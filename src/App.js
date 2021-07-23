e8e1f16a784b8504ee29262fabc1a9f6cd50d3a9
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-23 15:34:39 -0500

    Feat: Implementa lectura de estado de contexto de usuario en componente home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from '@utils/components/navbar/navbar';
 import Routes from '@utils/routes/routes';
+import { UserProvider } from '@contexts/UserContext/UserContext';
 
 function App() {
   return (
     <>
+    <UserProvider>
       <Navbar></Navbar>
       <Routes></Routes>
+    </UserProvider>
     </>
   );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,10 @@
-import { React } from 'react';
+import { React, useContext } from 'react';
+import { UserContext } from '@contexts/UserContext/UserContext';
 
 export default function Home(){
+    const { Name } = useContext(UserContext)
+    
     return (
-        <div>Hello World</div>
+        <div>Hello { Name }</div>
     );
 }
