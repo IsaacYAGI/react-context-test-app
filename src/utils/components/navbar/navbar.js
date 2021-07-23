15f15081682f571ee917a36dc0d39d188e75abd0
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-23 13:16:49 -0500

    Fix: Agrega clase active cuando seleccionas la ruta en cuestion

--- src/utils/components/navbar/navbar.js
+++ src/utils/components/navbar/navbar.js
@@ -1,28 +1,28 @@
 import { React } from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 export default function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link to="/home" className="navbar-brand" href="#">Test app</Link>
+          <NavLink to="/home" className="navbar-brand" href="#">Test app</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link to="/home" className="nav-link" aria-current="page" href="#">Home</Link>
+                <NavLink to="/home" className="nav-link" activeClassName='active' aria-current="page" href="#">Home</NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/about" className="nav-link" href="#">About</Link>
+                <NavLink to="/about" className="nav-link" href="#">About</NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/contacts" className="nav-link" href="#">Contacts</Link>
+                <NavLink to="/contacts" className="nav-link" href="#">Contacts</NavLink>
               </li>
               {/* <li className="nav-item">
-                <Link to="/" className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+                <NavLink to="/" className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</NavLink>
               </li> */}
             </ul>
           </div>
